35a0f6658f1b363cd0aa513d753b528f7fd3c199
pradhapganesanjava <pradhapganesanjava@gmail.com>
2016-08-01 19:38:26 -0400

    algorithm update desk

--- Algorithm/src/com/pg/algorithm/string/AnagramCheck.java
+++ Algorithm/src/com/pg/algorithm/string/AnagramCheck.java
@@ -9,18 +9,37 @@ public class AnagramCheck {
 	 */
 	public static void main(String[] args) {
 
+		System.out.println("isAnagram ? " + isAnagram("tea", "ate"));
 
 	}
 
-	
+	private static boolean isAnagram(String str1, String str2) {
+		Comparator comp = new AnagramComparable();
+		int intVal = comp.compare(str1, str2);
+		if (intVal != 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 }
-class AnagramComparable implements Comparator{
-	
+
+class AnagramComparable implements Comparator {
+
+	int[] charIntArr = new int[256];
+
 	@Override
 	public int compare(Object o1, Object o2) {
-	
+		String str1 = o1.toString();
+		String str2 = o1.toString();
 		
-		
+		char[] charArr1 = str1.toCharArray();
+		for(int i=0;i<charArr1.length;i++){
+			if(charIntArr[charArr1[i]] == '\u0000'){
+				charIntArr[charArr1[i]]++;
+			}
+		}
 		
 		return 0;
 	}
--- Algorithm/src/com/pgexample/algorithm/FibonacciSeq_Recur.java
+++ Algorithm/src/com/pgexample/algorithm/FibonacciSeq_Recur.java
@@ -8,8 +8,25 @@ public class FibonacciSeq_Recur {
 	public static void main(String[] args) {
 		int size = 10;
 		System.out.println("fib " + fibonacciRecur(size));
+		int[] fibIntArr = new int[100];
 	}
 
+	private static int fibonacciWithStorage(int[] fibIntArr, int fibInt) {
+		if (fibInt < 2) {
+			fibIntArr[0]=fibIntArr[1]=1;
+			return 1;
+		}
+		if (fibIntArr[fibInt] > 0) {
+			return fibIntArr[fibInt];
+		} else {
+			int newVal = fibonacciWithStorage(fibIntArr, fibInt - 1) + fibonacciWithStorage(fibIntArr, fibInt - 2);
+			fibIntArr[fibInt] = newVal;
+			return newVal;
+		}
+	}
+
+	// fib(i) repetitive many times... this is NOT recommended solution
+	// nth takes O (2^n)
 	private static int fibonacciRecur(int size) {
 		if (size == 1) {
 			return 0;
