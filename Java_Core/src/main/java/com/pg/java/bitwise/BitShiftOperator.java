43cb93f718b03f8c89bdfa456caf5d34976801bd
pradhapganesanjava <pradhap.ganesan.java@gmail.com>
2016-11-24 06:02:22 -0500

    sync

--- Java_Core/src/main/java/com/pg/java/bitwise/BitShiftOperator.java
+++ Java_Core/src/main/java/com/pg/java/bitwise/BitShiftOperator.java
@@ -13,28 +13,36 @@ public class BitShiftOperator {
 		System.out.println(val & bitmask);
 
 		int number = 8; // 0000 1000
+		int result = 0;
 		System.out.println("Original number : " + number);
 
 		// left shifting bytes with 1 position
-		number = number << 1; // should be 16 i.e. 0001 0000
+		result = number << 1; // should be 16 i.e. 0001 0000
 
 		// equivalent of multiplication of 2
-		System.out.println("value of number after left shift: " + number);
+		System.out.println("LEFT SHIFT: "+number+" << 1  = " + result);
 
 		number = -8;
 		// right shifting bytes with sign 1 position
-		number = number >> 1; // should be 16 i.e. 0001 0000
+		result = number >> 1; // should be 16 i.e. 0001 0000
 
 		// equivalent of division of 2
-		System.out.println("value of number after right shift with sign: " + number);
+		System.out.println("RIGHT-SIGNED SHIFT: "+number+" >> 1 = "+ result);
 
 		number = -8;
-		// right shifting bytes without sign 1 position
-		number = number >>> 1; // should be 16 i.e. 0001 0000
+		// right shifting bytes with sign 1 position
+		result = number >>> 1;
 
 		// equivalent of division of 2
-		System.out.println("value of number after right shift with sign: " + number);
+		System.out.println("RIGHT-UN-SIGNED SHIFT  (-Ve): "+number+" >>> 1 = "+ result);
 
+		number = 8;
+		// right shifting bytes with sign 1 position
+		result = number >>> 1;
+
+		// equivalent of division of 2
+		System.out.println("RIGHT-UN-SIGNED SHIFT (+Ve): "+number+" >>> 1 = "+ result);
+		
 	}
 
 }
