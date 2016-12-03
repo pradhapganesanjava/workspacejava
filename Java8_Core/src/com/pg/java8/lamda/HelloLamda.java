0f339b62203eb528087ea533291e61a029ac6a0d
pradhap <pganesan@tracfone.com>
2016-12-02 19:29:35 -0500

    java8

--- Java8_Core/src/com/pg/java8/lamda/HelloLamda.java
+++ Java8_Core/src/com/pg/java8/lamda/HelloLamda.java
@@ -4,26 +4,48 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
+ /*
+  * Lamda expression means:
+  * 	Lamda expression contains two parts
+  * 	 1) variable that points to implementation
+  * 	 2) the implementation uses the variable
+ 	> Internal method knows, what's parameter type (FUNCTIONAL INTERFACE) it takes, so it skips to create instance; 
+ 	> body of the method by passing parameters; 
+ 	> totally new to Java - an variable can be declared without mention about its type
+ */
 public class HelloLamda {
 
 	public static void main(String[] args) {
 
 		List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5);
 
-		//Internal Loop - sending consumer to loop by internal
+		//Legacy expression for internal loop forEach
+		intList.forEach(new ConsumerImpl());
+		
+		//Anonymous expression for internal loop forEach
 		intList.forEach(new Consumer<Integer>() {
 			@Override
 			public void accept(Integer t) {
-				System.out.println("int : " + t);
+				System.out.println("Anonymous : " + t);
 			}
 		});
 
+		//Anonymoun with Lamda
+		Consumer<Integer> intConsum = (Integer intVal) -> System.out.println("Anonymoun with Lamda " +intVal);
+		intList.forEach(intConsum);
 		
-		// Lamda rule: if an interface has only one method then simply invoke
-		// body of the method by passing parameters; 
-		// totally new to Java - an variable can be declared without mention about its type
-		//
+		//Lamda expression for internal loop forEach
+		//forEach internal loop assign every value into variable, and use the variable later.
 		intList.forEach(intWrap->System.out.println(intWrap));
-	}
 
+		Integer intWrap = 100;
+		System.out.println(" intWrap "+ intWrap);
+	}
+}
+
+class ConsumerImpl implements Consumer<Integer>{
+	@Override
+	public void accept(Integer t) {
+		System.out.println("ConsumerImpl "+t);
+	}
 }
---
+++ Java8_Core/src/com/pg/java8/methodref/MethodRef.java
@@ -0,0 +1,33 @@
+package com.pg.java8.methodref;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Before Java8 there were PASS BY VALUE, PASS BY REFERENCE
+ * now, We have PASS BY METHOD REFERENCE 
+ * pass by method reference uses new syntax ::
+ * 
+ */
+public class MethodRef {
+
+	public static void main(String[] args) {
+
+		List<Integer> intList = Arrays.asList(1,2,3,4);
+		
+		//instead of calling the method, reference is passed to other method (forEach)
+		intList.forEach(PassByMethodRef::myMethod);
+		
+	}
+
+	public static void myMethod(Integer i) {
+		System.out.println("myMethod is called :: " + i);
+	}
+}
+
+class PassByMethodRef {
+
+	public static void myMethod(int i) {
+		System.out.println("myMethod is called :: " + i);
+	}
+}
