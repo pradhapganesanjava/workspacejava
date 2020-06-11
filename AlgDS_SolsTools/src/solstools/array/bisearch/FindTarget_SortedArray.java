34c0a72018846f80405b6723224c361a9aaa35f6
pradhapganesanjava <pradhapganesanjava@gmail.com>
2020-06-11 01:20:56 -0400

    sync

--- AlgDS_SolsTools/src/solstools/array/bisearch/FindTarget_SortedArray.java
+++ AlgDS_SolsTools/src/solstools/array/bisearch/FindTarget_SortedArray.java
@@ -2,6 +2,23 @@ package solstools.array.bisearch;
 
 import java.util.Arrays;
 
+/**
+ * 
+35. Search Insert Position
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You may assume no duplicates in the array.
+
+Input: [1,3,5,6], 5	Output: 2
+
+Input: [1,3,5,6], 2 	Output: 1
+
+Input: [1,3,5,6], 7	Output: 4
+
+Input: [1,3,5,6], 0	Output: 0
+ *
+ */
+
 public class FindTarget_SortedArray {
 	static FindTarget_SortedArray_Solution sol;
 
